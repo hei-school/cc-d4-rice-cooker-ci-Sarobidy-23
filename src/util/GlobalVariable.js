27390d94faa5288937b862477443b4b708794930
Fanomezana Sarobidy RAKOTOMAHEFA <hei.sarobidy@gmail.com>
2023-12-11 16:29:57 +0300

    feat: global variable

---
+++ src/util/GlobalVariable.js
@@ -0,0 +1,8 @@
+export const ingredientExist = {
+  rice: {
+    waterAbsorptionRate: 0.02, //0.02 litre d'eau pour 1kg toute les DELTA_TIME qui est 2s
+    waterNeed: 1, //1 litre d'eau absorbé 1 kg de riz / time: 150s -> 1kg
+  },
+};
+
+export const DELTA_TIME = 2000; //milliseconde
