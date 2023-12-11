e6baf6a0aa4b61237efba088fe38ab57a11189ae
Fanomezana Sarobidy RAKOTOMAHEFA <hei.sarobidy@gmail.com>
2023-12-11 16:28:44 +0300

    chore: initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  includes: ["src/*.js"],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
